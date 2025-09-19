c8a79084651e8b6df9957a2c3f9f06a70c3bfc53
logojo <joel_88_7@hotmail.com>
2025-09-19 15:25:01 -0600

    instalacion de paquete para microservicios

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,6 +4,7 @@ import * as joi from 'joi';
 
 interface EnvVars{
     PORT: number;
+    NATS_SERVERS: string[];
     STRIPE_SECRET: string;
     STRIPE_SUCCESS_URL: string;
     STRIPE_CANCEL_URL: string;
@@ -12,13 +13,17 @@ interface EnvVars{
 
 const envsSchema = joi.object({
     PORT: joi.number().required(),
+    NATS_SERVERS: joi.array().items( joi.string() ).required(),
     STRIPE_SECRET: joi.string().required(),
     STRIPE_SUCCESS_URL: joi.string().required(),
     STRIPE_CANCEL_URL: joi.string().required(),
     STRIPE_ENDPOINT_SECRET: joi.string().required(),
 }).unknown(true);
 
-const { error, value } = envsSchema.validate( process.env );
+const { error, value } = envsSchema.validate({ 
+    ...process.env,
+    NATS_SERVERS: process.env.NATS_SERVERS.split(',') //convirtiendo la variable en un array
+});
 
 if( error ) {
     throw new Error(`Config validation error: ${ error.message }`);
@@ -28,6 +33,7 @@ const envVars : EnvVars = value;
 
 export const envs = {
     port: envVars.PORT,
+    natsServers: envVars.NATS_SERVERS,
     stripeSecret: envVars.STRIPE_SECRET,
     stripeSuccessUrl: envVars.STRIPE_SUCCESS_URL,
     stripeCancelUrl: envVars.STRIPE_CANCEL_URL,
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 import { AppModule } from './app.module';
 import { envs } from './config';
@@ -17,7 +18,18 @@ async function bootstrap() {
       forbidNonWhitelisted: true, 
     }) 
   );
-  
+
+  //*configuracion para convertir esta app hibrida (API/Micorservicios)
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.NATS,
+    options: {
+      servers: envs.natsServers
+    }
+  });
+
+  //*iniciando los microservicios
+  await app.startAllMicroservices();
+
   await app.listen(envs.port);
   logger.log(`Payments Microservice running on port ${ envs.port}`)
 }
