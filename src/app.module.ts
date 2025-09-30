6408a91f1603189079476e2b678874acd9235ccc
logojo <joel_88_7@hotmail.com>
2025-09-30 13:04:10 -0600

    feat: add health check module and controller for service status monitoring

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PaymentsModule } from './payments/payments.module';
+import { HealtCheckModule } from './healt-check/healt-check.module';
 
 @Module({
-  imports: [PaymentsModule],
-  controllers: [],
+  imports: [PaymentsModule, HealtCheckModule],
   providers: [],
 })
 export class AppModule {}
